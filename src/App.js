1272ad8c81c9a3712f5e984d8c6df1b5cb27088e
Angie To <angie.to@berkeley.edu>
2018-12-14 12:23:17 -0800

    basic demo with state (hard code version)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import Person from './components/Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Otter', age: 2},
+      {name: 'Cat', age: 3}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <Person name="Angie" />
-        <Person name="Sea"> I am the props.children </Person>
-        <Person name="Cattie" />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
       </div>
     );
   }
