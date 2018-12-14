18ae6a3f6a3d2e3720a06e67b46a98cf02bd3ea7
Angie To <angie.to@berkeley.edu>
2018-12-14 14:03:39 -0800

    passed method references between components

--- src/App.js
+++ src/App.js
@@ -11,16 +11,26 @@ class App extends Component {
   }
 
   // method
-  switchNameHandler = () => {
-    console.log("Was clicked");
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        {name: newName, age: 2},
+        {name: 'Cat', age: 3}
+      ]
+    });
   }
 
   render() {
     return (
       <div className="App">
-      <button onClick={ this.switchNameHandler }> Switch Name </button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      {/* One way to "bind" this to the event handler: give it an arrow function! */}
+      <button onClick={() => this.switchNameHandler("Hallo~~~") }> Switch Name </button>
+        <Person name={this.state.persons[0].name} 
+                age={this.state.persons[0].age} 
+                click={this.switchNameHandler.bind(this, "CHANGED!")} /> 
+                {/* Pass the event handler to the Person component, using another "bind" method */}
+                {/* As a result, when the user click this paragraph, its name property is changed */}
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
       </div>
     );
   }
